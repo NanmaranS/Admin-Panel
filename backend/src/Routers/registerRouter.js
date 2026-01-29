c15efc730e98b0b04717c7b16aeceff1aeb48660
demo <fake123@gmial.com>
2026-01-29 15:43:59 +0530

    Fullstack code push: frontend + backend + auth

--- backend/src/Routers/registerRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {register} from '../Controllers/registerController.js'
-
-const regRouter=express.Router()
-
-regRouter.post('/',register)
-
-export default regRouter
--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from './Pages/Login'
-export default function App() {
-  return (
-    <div>
-      <Login/>
-    </div>
-  )
-}
